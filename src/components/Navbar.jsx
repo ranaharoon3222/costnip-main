6dbd3eeb7f6e6cf81fea4a93b6be96ddef0a0d99
ranaharoon3222 <ranaharoon3222@gmail.com>
2023-05-24 12:31:29 +0500

    fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
-import logo from '../assets/logo.png';
+import logo from '@/assets/logo.png';
 import Link from 'next/link';
 import styles from '@/styles/styles';
 import Button from './Button';
