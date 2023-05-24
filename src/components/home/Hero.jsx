ba771be997a6cf1dc7cde56eabbc036eac4820b9
ranaharoon3222 <ranaharoon3222@gmail.com>
2023-05-24 12:33:11 +0500

    fxed

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,17 +1,16 @@
-import React, { useState, useEffect } from "react";
-import Image from "next/image";
-import styles from "@/styles/styles";
-import hero from "../../assets/hero-img.png";
-import { BsSearch } from "react-icons/bs";
-import { MdKeyboardArrowDown } from "react-icons/md";
-import { AiOutlineSearch } from "react-icons/ai";
-import plusIcon from "../../assets/plus-icon.png";
-import {Location} from "../location";
-import { getZipCode } from "../getzipCode"; 
-
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
+import styles from '@/styles/styles';
+import hero from '../../assets/hero-img.png';
+import { BsSearch } from 'react-icons/bs';
+import { MdKeyboardArrowDown } from 'react-icons/md';
+import { AiOutlineSearch } from 'react-icons/ai';
+import plusIcon from '../../assets/plus-icon.png';
+import { Location } from '../location';
+import { getZipCode } from '../getzipCode';
 
 const Hero = () => {
-  const [inputData, setInputData] = useState("");
+  const [inputData, setInputData] = useState('');
   const { latitude, longitude } = Location();
   ///////////////////////GETZIPCODE////////////////////////////
   const [zipCode, setZipCode] = useState(null);
@@ -33,7 +32,6 @@ const Hero = () => {
   }, [inputData]);
 
   const fetchData = async () => {
-
     try {
       const response = await fetch(
         `https://cnbackend.appspot.com/search?key=AIzaSyCK-zbsEAEkwSHSBMG6qJG9S121VAH_ArU&search=${inputData}&radius=2000&location=${Math.floor(
@@ -43,7 +41,7 @@ const Hero = () => {
       const data = await response.json();
       setData(data);
     } catch (error) {
-      console.error("Error fetching data:", error);
+      console.error('Error fetching data:', error);
     }
   };
 
@@ -59,54 +57,50 @@ const Hero = () => {
       className={`${styles.width} md:py-12 py-16 px-7 md:px-0 bg-darkBlue md:rounded-[20px]`}
     >
       {/* /////////////////////////////// */}
-      {zipCode ? (
-        <p>User's ZIP code: {zipCode}</p>
-      ) : (
-        <p>Loading ZIP code...</p>
-      )}
+      {zipCode ? <p>Users ZIP code: {zipCode}</p> : <p>Loading ZIP code...</p>}
       {/* /////////////////////////////// */}
-      <div className="md:flex items-center">
-        <div className="md:pl-28">
-          <h1 className="text-white font-bold leading-[50px] md:leading-[70px] text-[40px] md:text-[65px]">
-            <span className="text-primary">Never Overpay</span> <br /> For
+      <div className='items-center md:flex'>
+        <div className='md:pl-28'>
+          <h1 className='text-white font-bold leading-[50px] md:leading-[70px] text-[40px] md:text-[65px]'>
+            <span className='text-primary'>Never Overpay</span> <br /> For
             Healthcare <br /> Again
           </h1>
-          <p className="md:text-xl leading-[23px] text-[#CFD1D1] md:w-[85%] pt-4 pb-5">
+          <p className='md:text-xl leading-[23px] text-[#CFD1D1] md:w-[85%] pt-4 pb-5'>
             Compare costs and recommendations to lower cost medical, dental, and
             vision care in your local area
           </p>
         </div>
-        <div className="hidden md:block pr-10">
-          <Image src={hero} alt="" className="w-[530px]" />
+        <div className='hidden pr-10 md:block'>
+          <Image src={hero} alt='' className='w-[530px]' />
         </div>
       </div>
 
-      <div className="md:w-[65%] m-auto">
-        <form action="" className="md:flex w-full] relative">
-          <div className="absolute top-[14px] md:top-[19px] left-[15px] text-[#9E9E9E]">
-            <AiOutlineSearch className="text-xl" />
+      <div className='md:w-[65%] m-auto'>
+        <form action='' className='md:flex w-full] relative'>
+          <div className='absolute top-[14px] md:top-[19px] left-[15px] text-[#9E9E9E]'>
+            <AiOutlineSearch className='text-xl' />
           </div>
           <input
-            type="text"
+            type='text'
             value={inputData}
             onChange={handleChange}
-            placeholder="Enter Symptoms, services or code"
-            className="w-full text-sm md:text-base px-10 py-3 md:py-4 mb-3 md:mb-0 border-radius1 text-[#616161] border-r-2 border-solid border-[#ececec]"
+            placeholder='Enter Symptoms, services or code'
+            className='w-full text-sm md:text-base px-10 py-3 md:py-4 mb-3 md:mb-0 border-radius1 text-[#616161] border-r-2 border-solid border-[#ececec]'
           />
           <input
-            type="text"
-            placeholder="Enter zipcode"
-            className="w-full text-sm md:text-base px-10 py-3 md:py-4 mb-2 md:mb-0 border-radius1 border-radius2 text-[#616161] "
+            type='text'
+            placeholder='Enter zipcode'
+            className='w-full text-sm md:text-base px-10 py-3 md:py-4 mb-2 md:mb-0 border-radius1 border-radius2 text-[#616161] '
           />
           <button
-            type="submit"
-            className="hidden md:block bg-[#443CF4] px-5 py-5 rounded-2xl absolute top-0 right-[-15px]"
+            type='submit'
+            className='hidden md:block bg-[#443CF4] px-5 py-5 rounded-2xl absolute top-0 right-[-15px]'
           >
-            <BsSearch className="text-white text-lg" />
+            <BsSearch className='text-lg text-white' />
           </button>
         </form>
 
-        <ul className="bg-white text-black rounded-xl  mt-2 absolute z-[100]">
+        <ul className='bg-white text-black rounded-xl  mt-2 absolute z-[100]'>
           {data.map((ele, ind) => {
             console.log(ind);
 
@@ -123,7 +117,7 @@ const Hero = () => {
             return (
               <div key={ind}>
                 <li
-                  className="border-b-2 px-4 py-2 cursor-pointer "
+                  className='px-4 py-2 border-b-2 cursor-pointer '
                   onClick={() => handleGetData(ele.service_code)}
                 >
                   {ele.service_code}
@@ -133,15 +127,15 @@ const Hero = () => {
           })}
         </ul>
 
-        <div className="flex items-center gap-4 mt-2">
-          <div className="flex items-center gap-1">
-            <Image src={plusIcon} alt="" className="w-[20px]" />
+        <div className='flex items-center gap-4 mt-2'>
+          <div className='flex items-center gap-1'>
+            <Image src={plusIcon} alt='' className='w-[20px]' />
             <span className="text-white font-['Roboto'] text-sm font-normal">
               Medical
             </span>
           </div>
           <div>
-            <MdKeyboardArrowDown className="text-white text-2xl" />
+            <MdKeyboardArrowDown className='text-2xl text-white' />
           </div>
         </div>
       </div>
