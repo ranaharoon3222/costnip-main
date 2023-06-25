180f62c69e86f9365f8751be12fccb30ab8115c6
ranaharoon3222 <ranaharoon3222@gmail.com>
2023-06-25 12:47:10 +0500

    fic

--- src/pages/blogs/[blogid].js
+++ src/pages/blogs/[blogid].js
@@ -1,7 +1,7 @@
-import Loading from "@/components/loding";
-import { useFetcher } from "@/helpers/fetch";
-import { useMedia } from "@/helpers/useMedia";
-import { useRouter } from "next/router";
+import Loading from '@/components/loding';
+import { useFetcher } from '@/helpers/fetch';
+import { useMedia } from '@/helpers/useMedia';
+import { useRouter } from 'next/router';
 
 const Blog = () => {
   const router = useRouter();
@@ -9,25 +9,33 @@ const Blog = () => {
 
   const { getUrl } = useMedia();
 
-  const { data, isLoading, error } = useFetcher("/api/blogs/" + blogid + "?populate=*");
+  const { data, isLoading, error } = useFetcher(
+    '/api/blogs/' + blogid + '?populate=*'
+  );
 
   if (isLoading) return <Loading />;
   if (error) return <h1>Error</h1>;
 
-  console.log('latest',data);
+  console.log('latest', data);
 
   return (
     <>
-<div className="w-full max-w-screen-lg mx-[auto] ">
+      <div className='w-full max-w-screen-lg mx-[auto] '>
+        <h1 className='px-2 text-2xl font-bold md:px-6'>
+          {data.data.attributes.title}
+        </h1>
 
-  <h1 className="px-2 md:px-6 font-bold text-2xl">{data.data.attributes.title}</h1>
+        <img
+          className='py-6 w-[1000px] object-cover h-[500px] mx-[auto] '
+          src={getUrl(data.data.attributes.thumbnail)}
+          alt='image'
+        />
+      </div>
 
-      <img className="py-6 w-[1000px] object-cover h-[500px] mx-[auto] " src= {getUrl(data.data.attributes.thumbnail)} alt="image" /> 
-</div>
-          
-      <div className="w-full max-w-screen-lg mx-[auto] pt-6 pb-12 px-2 md:px-8"
-      dangerouslySetInnerHTML={{__html: data.data.attributes.text}}
-    />
+      <div
+        className='w-full max-w-screen-lg mx-[auto] pt-6 pb-12 px-2 md:px-8'
+        dangerouslySetInnerHTML={{ __html: data.data.attributes.text }}
+      />
     </>
   );
 };
