9b042dea6ac4712f2a4036f63ba8c2b6e8fa8735
Muhammad-Umar42 <umrking77@gmail.com>
2023-06-27 04:07:43 -0700

    fixed

--- src/pages/blogs/[blogid].js
+++ src/pages/blogs/[blogid].js
@@ -2,6 +2,8 @@ import Loading from "@/components/loding";
 import { useFetcher } from "@/helpers/fetch";
 import { useMedia } from "@/helpers/useMedia";
 import { useRouter } from "next/router";
+import { TwitterShareButton, TwitterIcon } from 'react-share';
+import { FacebookShareButton, FacebookIcon } from 'react-share';
 
 const Blog = () => {
   const router = useRouter();
@@ -29,6 +31,24 @@ const Blog = () => {
 
       <img className="py-6 w-[1000px] object-cover h-[500px] mx-[auto] " src= {getUrl(data.data.attributes.thumbnail)} alt="image" /> 
 </div>
+<div className="fixed bottom-10 left-10">
+      <FacebookShareButton
+        url={location.href}
+        quote={'Dummy text!'}
+        hashtag="#muo"
+        className="mr-2"
+      >
+        <FacebookIcon size={32} round />
+      </FacebookShareButton>
+
+      <TwitterShareButton
+  url={'location.href'}
+  quote={'Dummy text!'}
+  hashtag="#muo"
+>
+  <TwitterIcon size={32} round />
+</TwitterShareButton>
+    </div>
           <div className="blogs">
       <div className="w-full max-w-screen-lg mx-[auto] pt-6 pb-12 px-2 md:px-8"
       dangerouslySetInnerHTML={{__html: data.data.attributes.text}}
